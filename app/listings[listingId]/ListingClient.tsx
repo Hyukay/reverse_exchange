c700b150ec9ae698d6061f2aea155c946094b38b
Anis <khalil-anis.zabat.1@ens.etsmtl.ca>
2023-05-17 14:14:57 -0400

    another try for deployment

--- app/listings[listingId]/ListingClient.tsx
+++ app/listings[listingId]/ListingClient.tsx
@@ -12,9 +12,9 @@ import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 
 import Container from "@/app/components/Container";
 import { categories } from "@/app/components/navbar/Categories";
-import ListingHead from "@/app/components/listings/ListingHead";
-import ListingInfo from "@/app/components/listings/ListingInfo";
-import ListingReservation from "@/app/components/listings/ListingReservation";
+import ListingHead from "@/app/components/listing/ListingHead";
+import ListingInfo from "@/app/components/listing/ListingInfo";
+import ListingReservation from "@/app/components/listing/ListingReservation";
 
 const initialDateRange = {
   startDate: new Date(),
