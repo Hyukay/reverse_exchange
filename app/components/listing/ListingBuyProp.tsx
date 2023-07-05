dd4ceacd3fd4a97a1fbe73cd3f4a87baa51c94b3
Anis <khalil-anis.zabat.1@ens.etsmtl.ca>
2023-07-05 10:26:39 -0400

    fetching data test

--- app/components/listing/ListingBuyProp.tsx
+++ app/components/listing/ListingBuyProp.tsx
@@ -10,9 +10,14 @@ import { Address, SafeUser } from "@/app/types";
 import { escrowABI } from "@/app/abis/Escrow";
 import { useWalletClient, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 import { write } from "fs";
-import { BsFillNutFill } from "react-icons/bs";
+import { BsFillNutFill, BsWindowSidebar } from "react-icons/bs";
 import { Role, EscrowData } from "@/app/types";
-import {utils} from 'ethers'
+import {ethers, providers, utils} from 'ethers'
+import { set } from "date-fns";
+import { getAddress } from "viem";
+import { hardhat } from "viem/chains";
+import { realestateABI } from "@/app/abis/RealEstate";
+
 
 
 interface ListingBuyProps {
@@ -27,7 +32,56 @@ const ListingBuy: React.FC<ListingBuyProps> = ({
   id,
 }) => {
 
-  const {address} = useAccount();
+  const {address, isConnected} = useAccount();
+  //const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null);
+
+  console.log('Current connected address', address)
+  console.log('Is Connected?', isConnected)
+  const [account, setAccount] = useState<Address | null>(null);
+  const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null);
+  const [escrow, setEsrcow] = useState<ethers.Contract | null>(null);
+  const [realEstate, setRealEstate] = useState<ethers.Contract | null>(null);
+
+  
+  
+      //setProvider(provider)
+  
+  /*const loadBlockchainData = async () => {
+    
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  console.log('PROVIDER', provider)
+  const escrow = new ethers.Contract('0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512', escrowABI, provider)
+  const realEstate = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', realestateABI, provider)
+  setEsrcow(escrow)
+  setRealEstate(realEstate)
+  const totalSupply = await realEstate.totalSupply()
+  console.log('TOTAL SUPPLY', totalSupply)
+  const escrowAmount = await escrow.escrowAmount(1)
+  console.log('ESCROW AMOUNT', escrowAmount)
+
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account)
+    }
+  )}
+  */
+
+  
+  let {data : seller, isError: sellerError, isLoading: sellerLoading, Error: errorS} = useContractRead({
+
+    address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+    abi: escrowABI,
+    functionName: 'getNumber',
+    watch: true,
+    chainId: 31337,
+    enabled: true, 
+    }) as any
+
+    console.log('SELLER', seller, sellerError, sellerLoading, errorS)
+
+    
 
   //To read the amount to deposit for the escrow
   let {data : escrowAmount } = useContractRead({
@@ -36,7 +90,7 @@ const ListingBuy: React.FC<ListingBuyProps> = ({
     functionName: 'isListed',
     args: [1],
     watch: true,
-    }) as any 
+    }) as any
 
     console.log('ESCROW AMOUNT', escrowAmount)
     //read the escrowAmount make it assignable to reactNode
@@ -51,10 +105,11 @@ const ListingBuy: React.FC<ListingBuyProps> = ({
     watch: true,
   }) as {data: EscrowData | undefined}
 
+
   let {data: totalSupply} = useContractRead({
     address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
     abi: escrowABI,
-    functionName:'totalSupply',
+    functionName:'getBalance',
   }) as any
 
   console.log('TOTAL SUPPLY', totalSupply)
@@ -66,7 +121,7 @@ const ListingBuy: React.FC<ListingBuyProps> = ({
     functionName: 'escrowAmount',
     args: [escrowAmount],
     account: address
-    
+
   })
 
   const {writeAsync: writeDeposit} = useContractWrite(depositEarnest)
@@ -103,7 +158,6 @@ const ListingBuy: React.FC<ListingBuyProps> = ({
     functionName:'updateInspectionStatus',
     args: [id,true],
     account: address
-
   })
 
   const {data: updateInspectionStatusData, writeAsync: writeInspectionStatus} = useContractWrite(updateInspectionStatus);
@@ -197,6 +251,7 @@ const ListingBuy: React.FC<ListingBuyProps> = ({
           buyHandler
        });
         break;
+        
     }
   }, [role,buyHandler, inspectHandler, lenderHandler,notaryHandler,sellerHandler ]);
 
