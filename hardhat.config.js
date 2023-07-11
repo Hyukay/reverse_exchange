9163ef156916787f4d7050688d0676af88c371c8
Anis <khalil-anis.zabat.1@ens.etsmtl.ca>
2023-07-11 16:29:49 -0400

    Modified hardhad.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,19 @@
-
-
+const { env } = require("process");
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-abi-exporter')
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const INFURA_API_KEY = env.INFURA_API_KEY;
+const SEPOLIA_PRIVATE_KEY = env.SEPOLIA_PRIVATE_KEY;
+
 module.exports = {
+
   solidity: "0.8.17",
-};
+  networks : {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/"`${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+      }
+    }
+}
