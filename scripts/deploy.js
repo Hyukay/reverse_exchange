399f1725c32ca2625dcbfbd8ab28a0a50fce9708
Anis <khalil-anis.zabat.1@ens.etsmtl.ca>
2023-07-11 16:27:24 -0400

    changed deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,19 +5,18 @@ const tokens = (n) => {
 }
 
 async function main() {
-  // Setup accounts
-  const [buyer, seller, inspector, lender] = await hre.ethers.getSigners()
   
+
+  // Setup accounts
+  const [seller] = await hre.ethers.getSigners()
   // Deploy Real Estate
   const RealEstate = await hre.ethers.getContractFactory('RealEstate')
   const realEstate = await RealEstate.deploy()
   await realEstate.deployed()
 
   // Show signers 
-  console.log(`Buyer: ${buyer.address}`)
+  
   console.log(`Seller: ${seller.address}`)
-  console.log(`Inspector: ${inspector.address}`)
-  console.log(`Lender: ${lender.address}\n`)
 
   console.log(`Deployed Real Estate Contract at: ${realEstate.address}`)
   console.log(`Minting 3 properties...\n`)
@@ -28,38 +27,15 @@ async function main() {
   }
 
   // Deploy Escrow
-  const Escrow = await hre.ethers.getContractFactory('Escrow')
+  const Escrow = await hre.ethers.getContractFactory('Escrow_v2')
   const escrow = await Escrow.deploy(
     realEstate.address,
     seller.address,
-    inspector.address,
-    lender.address
   )
 
   await escrow.deployed()
 
   console.log(`Deployed Escrow Contract at: ${escrow.address}`)
-  console.log(`Listing 3 properties...\n`)
-
-  for (let i = 0; i < 3; i++) {
-    // Approve properties...
-    let transaction = await realEstate.connect(seller).approve(escrow.address, i + 1)
-    await transaction.wait()
-  }
-
-  // Listing properties...
-  let transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
-  await transaction.wait()
-
-  transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
-  await transaction.wait()
-
-  transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
-  await transaction.wait()
-
-  if(await escrow.connect(buyer).isListed(1)) {
-    console.log(`Property 1 is listed.`)
-  }
 
   console.log(`Finished.`)
 }
