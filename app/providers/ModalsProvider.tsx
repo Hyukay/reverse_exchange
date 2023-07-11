594a6a37fa069524a39c150b480a8ac29efebf02
Anis <khalil-anis.zabat.1@ens.etsmtl.ca>
2023-07-11 16:22:13 -0400

    Removed useless walletconnectmodal

--- app/providers/ModalsProvider.tsx
+++ app/providers/ModalsProvider.tsx
@@ -4,7 +4,6 @@ import LoginModal from "../components/modals/LoginModal";
 import RegisterModal from "../components/modals/RegisterModal";
 import RentModal from "../components/modals/RentModal";
 import SearchModal from "../components/modals/SearchModal";
-import WalletConnectModal from "../components/modals/WalletConnectModal";
 
 const ModalsProvider = () => {
   return ( 
@@ -13,7 +12,6 @@ const ModalsProvider = () => {
       <RegisterModal />
       <SearchModal />
       <RentModal />
-      <WalletConnectModal />
     </>
    );
 }
