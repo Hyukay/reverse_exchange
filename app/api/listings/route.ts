6cefabdd58c8035625f12bd67645219f391995a8
Anis <khalil-anis.zabat.1@ens.etsmtl.ca>
2023-07-12 11:57:11 -0400

    Added ipfsUri creation when creating a property

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -2,11 +2,17 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
+import { ThirdwebStorage } from "@thirdweb-dev/storage";
+
+
 
 export async function POST(
   request: Request, 
 ) {
   const currentUser = await getCurrentUser();
+    
+  // First, instantiate the thirdweb IPFS storage
+  const storage = new ThirdwebStorage();
 
   if (!currentUser) {
     return NextResponse.error();
@@ -25,26 +31,59 @@ export async function POST(
     price,
    } = body;
 
+ 
+
+  // Check if all fields are filled
   Object.keys(body).forEach((value: any) => {
     if (!body[value]) {
       NextResponse.error();
     }
   });
 
-  const listing = await prisma.listing.create({
-    data: {
-      title,
-      description,
-      imageSrc,
-      category,
-      roomCount,
-      bathroomCount,
-      guestCount,
-      locationValue: location.value,
-      price: parseInt(price, 10),
-      userId: currentUser.id
-    }
-  });
+    //Image URI
+    // Here we get the IPFS URI of where our metadata has been uploaded
+    
+   const metadata = {
+
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location: location.value,
+    price: parseInt(price, 10),
+    userId: currentUser.id
+
+  };
+
+    const imageUri = await storage.upload(metadata.imageSrc);
+    console.log(imageUri)
+    //replace the imageSrc with the IPFS URI
+    metadata.imageSrc = imageUri;
+    console.log(metadata)
+
+    //Upload metadata to IPFS
+    const ipfsUri = await storage.upload(metadata);
+    console.log(ipfsUri)
+   
+
+    const listing = await prisma.listing.create({
+      data: {
+        title,
+        description,
+        imageSrc,
+        category,
+        roomCount,
+        bathroomCount,
+        guestCount,
+        ipfsUri: ipfsUri,
+        locationValue: location.value,
+        price: parseInt(price, 10),
+        userId: currentUser.id
+      }
+    });
 
   return NextResponse.json(listing);
 }
